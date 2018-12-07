65397fff682a165f34494d434871f8226bece8f3
rocioluct <45259463+rocioluct@users.noreply.github.com>
2018-12-07 16:46:07 +0100

    Add files via upload

---
+++ Juego.py
@@ -0,0 +1,25 @@
+print ("Te reto a un juego. Tenemos 13 piedras. Cada uno en su turno tiene que coger 1, 2 o 3. El juego consiste en no ser el último en coger la última piedra. ")
+
+total = 13
+vp = 4
+
+while total >=3:
+    juego13= input ("Es tu turno, cuántas coges?:")
+    try:
+        ijuego13=int(juego13)
+        if ijuego13 <= 3:
+            er = vp - ijuego13
+            total = total - ijuego13 - er
+            print ("Yo he cogido", er)
+            print ("Quedan", total)
+            continue
+
+        else:
+            print ("Por favor introduce un valor numérico entre el 1 y el 3: ")
+            continue
+
+    except:
+        print ("Por favor introduce un valor numérico que sea 1, 2 o 3: ")
+        continue
+    total= total - ijuego13
+print ("Lo siento sólo quedan", total, ". Has perdido!")
